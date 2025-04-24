f10df980b6e660842ded66f9e324cefffb4aa348
Jock Currie <jockcurrie@gmail.com>
2025-04-24 18:06:42 +0200

    first commit

---
+++ scripts/template.R
@@ -0,0 +1,37 @@
+#####################################################################################
+##
+## Script name: 
+##
+## Purpose of script:
+##
+## Author: 
+##
+## Date Created: 202y-mm-dd
+##
+##
+## Notes:
+##   
+##
+#####################################################################################
+### packages & functions
+
+# require(dplyr)
+# require(sf)
+# require(terra)
+
+# source("functions/packages.R")       # loads up all the packages we need
+
+#####################################################################################
+### settings
+
+### filenames & directories
+
+# options(scipen = 6, digits = 4) # prefer non-scientific notation
+
+#####################################################################################
+###
+
+#####################################################################################
+### unload packages
+
+# detach("package:xxx", unload=TRUE)
